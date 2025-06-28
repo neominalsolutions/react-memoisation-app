a5edfb54d6bc51104f238a2821a0a59771198269
neominal.solutions <neominal.solutions@gmail.com>
2025-06-28 14:20:02 +0300

    React Memo

--- src/pages/react.memo.tsx
+++ src/pages/react.memo.tsx
@@ -1,7 +1,41 @@
+import React from 'react';
+import { useState } from 'react';
+function Root() {
+	// root'a ait bir state
+	const [random, setRandom] = useState<number>(0); // render4
+	console.log('Root component rendered');
+	return (
+		<div>
+			<h1>Root</h1>
+			{/* <Child /> */}
+			<MemoizedChild value={random} />
+			<p>
+				Random number: <strong>{random}</strong>
+			</p>
+			<button onClick={() => setRandom(Math.random() * 100)}>Generate</button>
+		</div>
+	);
+}
+type ChildProps = {
+	value?: number;
+};
+function Child({ value }: ChildProps) {
+	console.log('Child component rendered');
+	return (
+		<div>
+			<h1>Child</h1>
+			<p>[{value !== undefined ? value : 'No value provided'}]</p>
+		</div>
+	);
+}
+// memoize edilmiş referansı döndürdük.
+const MemoizedChild = React.memo(Child);
+
 function ReactMemoDemo() {
 	return (
 		<>
 			<h2>React Memo</h2>
+			<Root />
 		</>
 	);
 }
