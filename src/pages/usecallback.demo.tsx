c0f45fa96848235dcb5b731baee5561f43d11f4b
neominal.solutions <neominal.solutions@gmail.com>
2025-06-28 15:14:28 +0300

    useCallback

--- src/pages/usecallback.demo.tsx
+++ src/pages/usecallback.demo.tsx
@@ -1,7 +1,57 @@
+import React, { useCallback } from 'react';
+
+// Not: Eğer bir child component içerisinde bir function prop varsa ve bu function prop her renderda yeni bir referans alır, bu child component her renderda yeniden render edilir.
+
+function Root() {
+	const [randomNumber, setRandomNumber] = React.useState(0);
+	console.log('Root component rendered');
+
+	const onItemClick = useCallback(() => {
+		console.log('Item clicked');
+	}, []);
+
+	return (
+		<>
+			<MemoizedChild
+				// onItemClick={() => console.log('Item clicked')}
+				// onItemClick={useCallback(() => console.log('Item clicked'), [])}
+				onItemClick={onItemClick}
+			/>
+			<button
+				onClick={() => {
+					setRandomNumber(Math.random());
+					console.log('Random Number:', randomNumber);
+				}}
+			>
+				Generate Random Number
+			</button>
+		</>
+	);
+}
+
+// Not:Child Component içinde bir function propsu Root componentten referans verecek ise bu durumda useCallback hook kullanarak gereksiz renderları önleyebiliriz.
+
+type ChildProps = {
+	onItemClick?: () => void;
+};
+function Child({ onItemClick }: ChildProps) {
+	console.log('Child component rendered');
+	return (
+		<>
+			<button onClick={onItemClick}>Click Me</button>
+		</>
+	);
+}
+
+const MemoizedChild = React.memo(Child);
+
 function ReactUseCallbackDemo() {
 	return (
 		<>
 			<h2> React UseCallbackDemo</h2>
+			<div>
+				<Root />
+			</div>
 		</>
 	);
 }
