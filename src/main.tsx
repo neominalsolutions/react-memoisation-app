a9e0a58856e8971233a72c65ec77ff13b86af68d
neominal.solutions <neominal.solutions@gmail.com>
2025-06-28 15:23:32 +0300

    React Lazy

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-import {
-	createBrowserRouter,
-	Link,
-	Outlet,
-	RouterProvider,
-} from 'react-router';
+import { createBrowserRouter, Link, RouterProvider } from 'react-router';
 
-import ReactMemoDemo from './pages/react.memo.tsx';
-import ReactUseCallbackDemo from './pages/usecallback.demo.tsx';
-import ReactUseMemoDemo from './pages/usememo.demo.tsx';
+// import ReactMemoDemo from './pages/react.memo.tsx';
+// import ReactUseCallbackDemo from './pages/usecallback.demo.tsx';
+// import ReactUseMemoDemo from './pages/usememo.demo.tsx';
+
+const ReactMemoDemo = React.lazy(() => import('./pages/react.memo.tsx'));
+const ReactUseMemoDemo = React.lazy(() => import('./pages/usememo.demo.tsx'));
+const ReactUseCallbackDemo = React.lazy(
+	() => import('./pages/usecallback.demo.tsx')
+);
+
+// import şeklimizi değiştirerek kodları farklı js dosyalarına ayırdık.
+// code splitting tekniği
+// react lazy ile dinamik import yaparak kodları parçalara ayırabiliriz.
 
 const router = createBrowserRouter([
 	{
